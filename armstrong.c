f1511f47b8f36e0c0c158843c96bb32ffb0530db
lmohanraj <35128040+lmohanraj@users.noreply.github.com>
2018-01-21 11:11:08 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n, actualNumber, remr, res=0;
+    printf("Enter a three digit integer: ");
+    scanf("%d", &n);
+    actualNumber = n;
+    while (actualNumber != 0)
+    {
+        remainder = actualNumber%10;
+        res+= rem*rem*rem;
+        actualNumber /= 10;
+    }
+    if(res==n)
+        printf("yes");
+    else
+        printf("no");
+    return 0;
+}
